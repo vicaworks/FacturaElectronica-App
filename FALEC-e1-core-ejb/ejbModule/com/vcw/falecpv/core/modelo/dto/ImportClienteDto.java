08ffacf4073638c51779f2076ee8ade7bd792e86
Jorge Toaza <50271634+jftoaza@users.noreply.github.com>
2020-04-12 19:07:21 -0500

    JT - agregar campos cliente

--- FALEC-e1-core-ejb/ejbModule/com/vcw/falecpv/core/modelo/dto/ImportClienteDto.java
+++ FALEC-e1-core-ejb/ejbModule/com/vcw/falecpv/core/modelo/dto/ImportClienteDto.java
@@ -24,6 +24,17 @@ public class ImportClienteDto implements Serializable {
 	private String direccion;
 	private String correoElectronico;
 	private String telefono;
+	private String estado;
+	private String nombreGarante1;
+	private String cedulaGarante1;
+	private String direccionGarante1;
+	private String telefonoGarante1;
+	private String ocupacionGarante1;
+	private String nombreGarante2;
+	private String cedulaGarante2;
+	private String direccionGarante2;
+	private String telefonoGarante2;
+	private String ocupacionGarante2;
 	private boolean error = false;
 	private String novedad;
 	private String idCliente;
@@ -172,5 +183,159 @@ public class ImportClienteDto implements Serializable {
 	 */
 	public void setIdCliente(String idCliente) {
 		this.idCliente = idCliente;
+	}
+
+	/**
+	 * @return the estado
+	 */
+	public String getEstado() {
+		return estado;
+	}
+
+	/**
+	 * @param estado the estado to set
+	 */
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+	/**
+	 * @return the nombreGarante1
+	 */
+	public String getNombreGarante1() {
+		return nombreGarante1;
+	}
+
+	/**
+	 * @param nombreGarante1 the nombreGarante1 to set
+	 */
+	public void setNombreGarante1(String nombreGarante1) {
+		this.nombreGarante1 = nombreGarante1;
+	}
+
+	/**
+	 * @return the cedulaGarante1
+	 */
+	public String getCedulaGarante1() {
+		return cedulaGarante1;
+	}
+
+	/**
+	 * @param cedulaGarante1 the cedulaGarante1 to set
+	 */
+	public void setCedulaGarante1(String cedulaGarante1) {
+		this.cedulaGarante1 = cedulaGarante1;
+	}
+
+	/**
+	 * @return the direccionGarante1
+	 */
+	public String getDireccionGarante1() {
+		return direccionGarante1;
+	}
+
+	/**
+	 * @param direccionGarante1 the direccionGarante1 to set
+	 */
+	public void setDireccionGarante1(String direccionGarante1) {
+		this.direccionGarante1 = direccionGarante1;
+	}
+
+	/**
+	 * @return the telefonoGarante1
+	 */
+	public String getTelefonoGarante1() {
+		return telefonoGarante1;
+	}
+
+	/**
+	 * @param telefonoGarante1 the telefonoGarante1 to set
+	 */
+	public void setTelefonoGarante1(String telefonoGarante1) {
+		this.telefonoGarante1 = telefonoGarante1;
+	}
+
+	/**
+	 * @return the ocupacionGarante1
+	 */
+	public String getOcupacionGarante1() {
+		return ocupacionGarante1;
+	}
+
+	/**
+	 * @param ocupacionGarante1 the ocupacionGarante1 to set
+	 */
+	public void setOcupacionGarante1(String ocupacionGarante1) {
+		this.ocupacionGarante1 = ocupacionGarante1;
+	}
+
+	/**
+	 * @return the nombreGarante2
+	 */
+	public String getNombreGarante2() {
+		return nombreGarante2;
+	}
+
+	/**
+	 * @param nombreGarante2 the nombreGarante2 to set
+	 */
+	public void setNombreGarante2(String nombreGarante2) {
+		this.nombreGarante2 = nombreGarante2;
+	}
+
+	/**
+	 * @return the cedulaGarante2
+	 */
+	public String getCedulaGarante2() {
+		return cedulaGarante2;
+	}
+
+	/**
+	 * @param cedulaGarante2 the cedulaGarante2 to set
+	 */
+	public void setCedulaGarante2(String cedulaGarante2) {
+		this.cedulaGarante2 = cedulaGarante2;
+	}
+
+	/**
+	 * @return the direccionGarante2
+	 */
+	public String getDireccionGarante2() {
+		return direccionGarante2;
+	}
+
+	/**
+	 * @param direccionGarante2 the direccionGarante2 to set
+	 */
+	public void setDireccionGarante2(String direccionGarante2) {
+		this.direccionGarante2 = direccionGarante2;
+	}
+
+	/**
+	 * @return the telefonoGarante2
+	 */
+	public String getTelefonoGarante2() {
+		return telefonoGarante2;
+	}
+
+	/**
+	 * @param telefonoGarante2 the telefonoGarante2 to set
+	 */
+	public void setTelefonoGarante2(String telefonoGarante2) {
+		this.telefonoGarante2 = telefonoGarante2;
+	}
+
+	/**
+	 * @return the ocupacionGarante2
+	 */
+	public String getOcupacionGarante2() {
+		return ocupacionGarante2;
+	}
+
+	/**
+	 * @param ocupacionGarante2 the ocupacionGarante2 to set
+	 */
+	public void setOcupacionGarante2(String ocupacionGarante2) {
+		this.ocupacionGarante2 = ocupacionGarante2;
 	}
 }
--- FALEC-e1-core-ejb/ejbModule/com/vcw/falecpv/core/modelo/persistencia/Cliente.java
+++ FALEC-e1-core-ejb/ejbModule/com/vcw/falecpv/core/modelo/persistencia/Cliente.java
@@ -36,11 +36,13 @@ public class Cliente implements Serializable{
 	@Column(name = "idcliente", nullable = false, length = 40)
 	private String idcliente;
 	
+	@Basic(optional = false)
 	@NotNull
 	@Size(min = 1, max = 20)
 	@Column(name = "identificacion", nullable = false, length = 20)
 	private String identificacion;
 	
+	@Basic(optional = false)
 	@NotNull
 	@Size(min = 1, max = 300)
 	@Column(name = "razonsocial", nullable = false, length = 300)
@@ -58,15 +60,63 @@ public class Cliente implements Serializable{
 	@Column(name = "telefono", length = 20)
 	private String telefono;
 	
+	@Basic(optional = false)
 	@NotNull
 	@Size(min = 1, max = 40)
 	@Column(name = "idusuario", nullable = false, length = 40)
 	private String idusuario;
 	
+	@Basic(optional = false)
 	@NotNull
     @Column(name = "updated", nullable = false)
     @Temporal(TemporalType.TIMESTAMP)
     private Date updated;
+	
+	@Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 1)
+    @Column(name = "estado", nullable = false, length = 1)
+    private String estado;
+	
+	@Size(max = 300)
+	@Column(name = "nombregarante1", length = 300)
+	private String nombregarante1;
+	
+	@Size(max = 20)
+	@Column(name = "cedulagarante1", length = 20)
+	private String cedulagarante1;
+	
+	@Size(max = 300)
+	@Column(name = "direcciongarante1", length = 300)
+	private String direcciongarante1;
+	
+	@Size(max = 20)
+	@Column(name = "telefonogarante1", length = 20)
+	private String telefonogarante1;
+	
+	@Size(max = 50)
+	@Column(name = "ocupaciongarante1", length = 50)
+	private String ocupaciongarante1;
+	
+	@Size(max = 300)
+	@Column(name = "nombregarante2", length = 300)
+	private String nombregarante2;
+	
+	@Size(max = 20)
+	@Column(name = "cedulagarante2", length = 20)
+	private String cedulagarante2;
+	
+	@Size(max = 300)
+	@Column(name = "direcciongarante2", length = 300)
+	private String direcciongarante2;
+	
+	@Size(max = 20)
+	@Column(name = "telefonogarante2", length = 20)
+	private String telefonogarante2;
+	
+	@Size(max = 50)
+	@Column(name = "ocupaciongarante2", length = 50)
+	private String ocupaciongarante2;
 	
 	@ManyToOne
     @JoinColumn(name = "idempresa", referencedColumnName = "idempresa", nullable = false)
@@ -248,5 +298,159 @@ public class Cliente implements Serializable{
 	 */
 	public void setTipoIdentificacion(TipoIdentificacion tipoIdentificacion) {
 		this.tipoIdentificacion = tipoIdentificacion;
+	}
+
+	/**
+	 * @return the estado
+	 */
+	public String getEstado() {
+		return estado;
+	}
+
+	/**
+	 * @param estado the estado to set
+	 */
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+	/**
+	 * @return the nombregarante1
+	 */
+	public String getNombregarante1() {
+		return nombregarante1;
+	}
+
+	/**
+	 * @param nombregarante1 the nombregarante1 to set
+	 */
+	public void setNombregarante1(String nombregarante1) {
+		this.nombregarante1 = nombregarante1;
+	}
+
+	/**
+	 * @return the cedulagarante1
+	 */
+	public String getCedulagarante1() {
+		return cedulagarante1;
+	}
+
+	/**
+	 * @param cedulagarante1 the cedulagarante1 to set
+	 */
+	public void setCedulagarante1(String cedulagarante1) {
+		this.cedulagarante1 = cedulagarante1;
+	}
+
+	/**
+	 * @return the direcciongarante1
+	 */
+	public String getDirecciongarante1() {
+		return direcciongarante1;
+	}
+
+	/**
+	 * @param direcciongarante1 the direcciongarante1 to set
+	 */
+	public void setDirecciongarante1(String direcciongarante1) {
+		this.direcciongarante1 = direcciongarante1;
+	}
+
+	/**
+	 * @return the telefonogarante1
+	 */
+	public String getTelefonogarante1() {
+		return telefonogarante1;
+	}
+
+	/**
+	 * @param telefonogarante1 the telefonogarante1 to set
+	 */
+	public void setTelefonogarante1(String telefonogarante1) {
+		this.telefonogarante1 = telefonogarante1;
+	}
+
+	/**
+	 * @return the ocupaciongarante1
+	 */
+	public String getOcupaciongarante1() {
+		return ocupaciongarante1;
+	}
+
+	/**
+	 * @param ocupaciongarante1 the ocupaciongarante1 to set
+	 */
+	public void setOcupaciongarante1(String ocupaciongarante1) {
+		this.ocupaciongarante1 = ocupaciongarante1;
+	}
+
+	/**
+	 * @return the nombregarante2
+	 */
+	public String getNombregarante2() {
+		return nombregarante2;
+	}
+
+	/**
+	 * @param nombregarante2 the nombregarante2 to set
+	 */
+	public void setNombregarante2(String nombregarante2) {
+		this.nombregarante2 = nombregarante2;
+	}
+
+	/**
+	 * @return the cedulagarante2
+	 */
+	public String getCedulagarante2() {
+		return cedulagarante2;
+	}
+
+	/**
+	 * @param cedulagarante2 the cedulagarante2 to set
+	 */
+	public void setCedulagarante2(String cedulagarante2) {
+		this.cedulagarante2 = cedulagarante2;
+	}
+
+	/**
+	 * @return the direcciongarante2
+	 */
+	public String getDirecciongarante2() {
+		return direcciongarante2;
+	}
+
+	/**
+	 * @param direcciongarante2 the direcciongarante2 to set
+	 */
+	public void setDirecciongarante2(String direcciongarante2) {
+		this.direcciongarante2 = direcciongarante2;
+	}
+
+	/**
+	 * @return the telefonogarante2
+	 */
+	public String getTelefonogarante2() {
+		return telefonogarante2;
+	}
+
+	/**
+	 * @param telefonogarante2 the telefonogarante2 to set
+	 */
+	public void setTelefonogarante2(String telefonogarante2) {
+		this.telefonogarante2 = telefonogarante2;
+	}
+
+	/**
+	 * @return the ocupaciongarante2
+	 */
+	public String getOcupaciongarante2() {
+		return ocupaciongarante2;
+	}
+
+	/**
+	 * @param ocupaciongarante2 the ocupaciongarante2 to set
+	 */
+	public void setOcupaciongarante2(String ocupaciongarante2) {
+		this.ocupaciongarante2 = ocupaciongarante2;
 	}
 }
