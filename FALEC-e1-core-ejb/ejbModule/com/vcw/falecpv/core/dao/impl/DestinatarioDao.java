a8fa79228ccb445b9dc398dbdeb9dce3b13174cd
isabel <marysabel0711@gmail.com>
2020-06-10 15:40:38 -0500

    ilo-merge

--- FALEC-e1-core-ejb/ejbModule/com/vcw/falecpv/core/dao/impl/DestinatarioDao.java
+++ FALEC-e1-core-ejb/ejbModule/com/vcw/falecpv/core/dao/impl/DestinatarioDao.java
@@ -3,12 +3,8 @@
  */
 package com.vcw.falecpv.core.dao.impl;
 
-import java.util.List;
-
 import javax.ejb.Stateless;
-import javax.persistence.Query;
 
-import com.servitec.common.dao.exception.DaoException;
 import com.vcw.falecpv.core.dao.AppGenericDao;
 import com.vcw.falecpv.core.modelo.persistencia.Destinatario;
 
@@ -24,20 +20,6 @@ public class DestinatarioDao extends AppGenericDao<Destinatario, String> {
 	 */
 	public DestinatarioDao() {
 		super(Destinatario.class);
-	}
-	
-	@SuppressWarnings("unchecked")
-	public List<Destinatario> getIdDestinatarioByCabeceraIdList(List<String> idsList)throws DaoException{
-		try {
-			
-			Query q = getEntityManager().createQuery("SELECT d FROM Destinatario d WHERE d.cabecera.idcabecera in :idsList ORDER BY d.cabecera.idcabecera,d.razonsocialdestinatario");
-			q.setParameter("idsList", idsList);
-			
-			return q.getResultList();
-			
-		} catch (Exception e) {
-			throw new DaoException(e);
-		}
 	}
 
 }
