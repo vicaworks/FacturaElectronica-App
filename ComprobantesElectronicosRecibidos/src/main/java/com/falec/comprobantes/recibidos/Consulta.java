ca460911e21df6fb71d2536a949470a7d5db361d
Jorge Toaza <50271634+jftoaza@users.noreply.github.com>
2020-08-27 23:27:54 -0500

    JT - Ventana

---
+++ ComprobantesElectronicosRecibidos/src/main/java/com/falec/comprobantes/recibidos/Consulta.java
@@ -0,0 +1,16 @@
+package com.falec.comprobantes.recibidos;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Consulta extends Application {
+
+	@Override
+	public void start(Stage primaryStage) {
+		
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
